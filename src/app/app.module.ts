9a95887ad900eecfea72677f1b5ecde5c685bc64
PVignos <pietro.vignola@gmail.com>
2023-01-31 16:05:51 +0100

    Feature/setup (#1)
    * @angular/material added
    
    * Firebase added
    
    * feat: editor configuration added
    
    * feat: lint conf added
    
    * feat: new assets
    
    * feat: adding main components
    
    * feat: recalculate integrity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import {AppFirebaseModule} from './shared/app-firebase/app-firebase.module';
+import {AppMaterialModule} from './shared/app-material/app-material.module';
+import {CreateComponent} from './components/create/create.component';
+import {FeedComponent} from './components/feed/feed.component';
+import {HeaderComponent} from './components/header/header.component';
+import {ProfileCardComponent} from './components/profile-card/profile-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    FeedComponent,
+    HeaderComponent,
+    ProfileCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppFirebaseModule,
+    AppMaterialModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-oauth-47d99',
+    appId: '1:911456654792:web:063aed2a6d8bf622535cfd',
+    storageBucket: 'angular-oauth-47d99.appspot.com',
+    apiKey: 'AIzaSyBqwsfXJD5YBCe2u6DrHu53aFTbU6YGdrM',
+    authDomain: 'angular-oauth-47d99.firebaseapp.com',
+    messagingSenderId: '911456654792',
+    measurementId: 'G-B2RZ4V2JXZ',
+  },
+  production: false,
+  name: 'test',
+  api: '' // host API
+};
